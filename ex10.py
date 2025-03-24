67644de74e094e6c6316e0e365669f79f818673d
Schutzz <lucas.schutz@aluno.fmpsc.edu.br>
2025-03-24 11:04:21 -0300

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,11 @@
+nome = "Lucas Schutz"
+
+print (nome[:])
+print (nome[:5])
+print (nome[6:])
+print (nome[0::2])
+print (nome[1::2])
+print (nome[:-1])
+print (nome[::-1])
+
+print(round(199.98333333,2))
---
+++ ex11.py
@@ -0,0 +1,9 @@
+nome = "Lucas"
+idade = 18
+altura = 1.77
+
+frase = "{0} tem {1} anos".format(nome, idade)
+frase2 = "{0} tem {1} anos e {0} mede {2}".format(nome, idade, altura)
+
+print (frase)
+print (frase2)
